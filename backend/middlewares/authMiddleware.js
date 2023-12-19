9ac69938aaf1c3c5edb6edc08ad384bc15d690d7
Aman Jain <ajinhindauncity@gmail.com>
2023-12-19 23:27:31 +0530

    backend error resolve

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const {
-  registerController,
-  loginController,
-  currentUserController,
-} = require("../controllers/authController");
-const authMiddelware = require("../middlewares/authMiddelware");
+const JWT = require("jsonwebtoken");
 
-const router = express.Router();
-
-//routes
-//REGISTER || POST
-router.post("/register", registerController);
-
-//LOGIN || POST
-router.post("/login", loginController);
-
-//GET CURRENT USER || GET
-router.get("/current-user", authMiddelware, currentUserController);
-
-module.exports = router;
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Auth Failed",
+        });
+      } else {
+        req.body.userId = decode.userId;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send({
+      success: false,
+      error,
+      message: "Auth Failedd",
+    });
+  }
+};
